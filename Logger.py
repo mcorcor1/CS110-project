b9f51d7c7d4b1f14f04db3b1a874ac72af9378da
Kevin Wallace <kjwall97@yahoo.com>
2017-04-21 02:22:27 -0400

    Added Logger.py

---
+++ Logger.py
@@ -0,0 +1,46 @@
+import time
+import os
+
+class Logger:
+    def __init__(self):
+        self.name = 'Logger'
+        self.init_time = time.strftime('%m-%d-%y (%H;%M;%S)')
+        self.cwd = os.getcwd()
+        self.filename = self.cwd + '/logs/ExplodingKittens Log ' + self.init_time + '.html'
+
+    # initialize the html file to hold the events
+    def initialize(self):
+        if not os.path.exists('%s/logs/' % self.cwd):
+            os.makedirs('%s/logs/' % self.cwd)
+        self.file = open(self.filename, 'w')
+        css = '''<style>
+        h1 {
+            font-size: 40px;
+        }
+        h2 {
+            font-size: 30px;
+        }
+        p {
+            font-size: 16px;
+        }
+        </style>'''
+        self.file.write(css)
+        self.file.write('<h1>Exploding Kittens Log</h1>\n')
+        self.file.write('<h2>%s</2>\n' % self.init_time)
+        self.log('Initialized log..', 'init')
+
+    # adds a new event to the log
+    def log(self, event, attribute):
+        self.file.write('<p class=%s>' % attribute + event + '</p>')
+
+    # closes the log file, to be used when the program is closed
+    def close(self):
+        self.file.close()
+
+''' This is test code
+logger = Logger()
+logger.initialize()
+logger.log('Card played', 'act')
+logger.log('Game exited..', 'exit')
+logger.close()
+'''
